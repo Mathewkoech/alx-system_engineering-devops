af9f2fc5562e6a4d9823b31c3168d8501953dfcf
mathewkoech <mathewkoech55@gmail.com>
2024-04-19 10:13:08 -0700

    a manifest that kills a process named killmenow.

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+#manifest that utilizes the exec resource to run the pkill command.
+exec { 'killing a process using pkill':
+  command  => 'pkill -9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
+}
